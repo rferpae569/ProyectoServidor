31a84feba69add32a25b80c7acc82c2678e3897c
rferpae569 <rferpae569@g.educaand.es>
2023-04-29 00:21:01 +0200

    ProyectorServidorRubenFernandez

--- elecciones/Victoria.php
+++ elecciones/Victoria.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="../icono/claquetaicon.png">
     <title>Victoria</title>
     <!-- <link rel="stylesheet" href="../bootstrap-5.2.3-dist/css/bootstrap.min.css">
-    <script src="bootstrap-5.2.3-dist/js/bootstrap.min.js"></script> -->
+    <script src="../bootstrap-5.2.3-dist/js/bootstrap.min.js"></script> -->
     <link rel="stylesheet" href="../css/EstiloFormulario.css">
 </head>
 <?php
--- elecciones/VictoriaDos.php
+++ elecciones/VictoriaDos.php
@@ -7,7 +7,7 @@
     <link rel="shortcut icon" href="../icono/claquetaicon.png">
     <title>Victoria</title>
     <!-- <link rel="stylesheet" href="../bootstrap-5.2.3-dist/css/bootstrap.min.css">
-    <script src="bootstrap-5.2.3-dist/js/bootstrap.min.js"></script> -->
+    <script src="../bootstrap-5.2.3-dist/js/bootstrap.min.js"></script> -->
     <link rel="stylesheet" href="../css/EstiloFormulario.css">
 </head>
 <?php
@@ -21,9 +21,9 @@
  
 ?>
 <body>
-        <h1>¡Enhorabuena, ha ganado <?php mostrarturno($_SESSION["usuario1"], $_SESSION["usuario2"]);?></h1>
-        <form action="VictoriaDos.php" method="post"><!--Creamos un formulario para cerrar la sesion.-->
-        <input type="submit" value="Cerrar Sesion" id="cerrar" name="cerrar">
-        </form>
+    <h1>¡Enhorabuena, ha ganado <?php mostrarturno($_SESSION["usuario1"], $_SESSION["usuario2"]);?></h1>
+    <form action="VictoriaDos.php" method="post"><!--Creamos un formulario para cerrar la sesion.-->
+    <input type="submit" value="Cerrar Sesion" id="cerrar" name="cerrar">
+    </form>
 </body>
 </html>
